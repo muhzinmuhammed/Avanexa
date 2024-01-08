f639bb192be8aa17929112e2e6d9d1322f55c987
muhzin sidiq <muhzinsidhiq333@gmail.com>
2024-01-08 14:06:02 +0530

    fix the error

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectToDb from './Connection/config.js';
 import userRouter from "./Routes/userRouer.js";
+import path from 'path'
 
 
 const app=express()
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, "dist")));
 
 /*student route*/
 app.use("/user", userRouter);
